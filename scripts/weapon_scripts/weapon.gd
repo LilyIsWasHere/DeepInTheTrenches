f7c26a132a1d49cea6d9c25357b8b5e3481f9930
SanPug1 <125669770+SanPug1@users.noreply.github.com>
2026-03-09 23:54:15 -0400

    weapon and bullet template

---
+++ scripts/weapon_scripts/weapon.gd
@@ -0,0 +1,44 @@
+extends Node3D
+
+@export var range : float = 10.0
+@export var damage : float = 5.0
+@export var affected_area : float = 1.0
+
+@export var mag_size : int = 30
+@export var ammo_in_mag : int = 30
+@export var ammo_in_reserve : int = 300
+@export var ammo_per_shot : int = 1
+
+const bullet : PackedScene = preload("res://scenes/bullet.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func reload() -> void:
+	var refill_amt : int = mag_size - ammo_in_mag
+	
+	# refills either the entire mag or as much as possible if there is not enough ammo in storage
+	if ammo_in_reserve >= refill_amt:
+		ammo_in_reserve -= refill_amt
+		ammo_in_mag += refill_amt
+	else:
+		refill_amt = ammo_in_reserve
+		ammo_in_reserve -= refill_amt
+		ammo_in_mag += refill_amt
+		
+func shoot() -> void:
+	if ammo_in_mag >= ammo_per_shot:
+		ammo_in_mag -= ammo_per_shot
+		
+		for i in range(ammo_per_shot):
+			var bullet_instance : Node3D = bullet.instantiate()
+			get_tree().current_scene.add_child(bullet_instance)
+		
+			#bullet_instance.shoot($Weapon.global_position, target_position)
+		
