97522951ec013289f24d9fa9fc24e867f860c37b
LilyIsWasHere <78503374+LilyIsWasHere@users.noreply.github.com>
2026-03-25 15:59:27 -0400

    unit LOS overhaul

--- line_of_sight_manager.gd
+++ line_of_sight_manager.gd
@@ -4,8 +4,8 @@ extends Node3D
 @export var max_raycasts_per_tick: int = 100
 
 var raycasts_this_tick: int = 0
-var last_los_idx_player: int = 0
-var last_los_idx_enemy: int = 0
+var last_los_idx_player: Array[int] = [0,0]
+var last_los_idx_enemy: Array[int] = [0,0]
 
 var unit_arrs: Array[Array] = []
 
@@ -13,7 +13,10 @@ var unit_last_seen_by: Dictionary[Unit, Unit]
 
 var unit_closest_visible_enemy: Dictionary[Unit, Unit]
 
-var units_seen_this_frame: Array[Array]
+var player_0_units_seen_this_frame: Dictionary[Unit, bool]
+var player_1_units_seen_this_frame: Dictionary[Unit, bool]
+
+var units_seen_this_frame: Array[Dictionary]
 
 # Called when the node enters the scene tree for the first time.
 func _init() -> void:
@@ -22,7 +25,7 @@ func _init() -> void:
 	unit_arrs.append(arr0)
 	unit_arrs.append(arr1)
 	
-	units_seen_this_frame = [[],[]]
+	units_seen_this_frame = [player_0_units_seen_this_frame, player_1_units_seen_this_frame]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -66,7 +69,7 @@ func check_los(a: Unit, b: Unit) -> bool:
 
 
 func get_enemy_unit_visibility(player_id: int) -> Array[Array]:
-	
+	var enemy_id: int = (player_id + 1) % 2
 	
 	var player_arr: Array = unit_arrs[player_id]
 	var enemy_arr: Array = unit_arrs[(player_id + 1) % 2]
@@ -76,17 +79,17 @@ func get_enemy_unit_visibility(player_id: int) -> Array[Array]:
 	
 	raycasts_this_tick = 0
 	
-	var last_e: int = last_los_idx_enemy
-	var last_p: int = last_los_idx_player
+	var last_e: int = last_los_idx_enemy[player_id]
+	var last_p: int = last_los_idx_player[player_id]
 	
 	var break_enemy: bool = false
 	var first_enemy_iter_this_tick: bool = true
 	
-	for i in range(last_los_idx_enemy, unit_arrs[1].size()):
+	for i in range(last_los_idx_enemy[player_id], unit_arrs[enemy_id].size()):
 
 		
 		last_e = i
-		var eu: Unit = unit_arrs[1][i]
+		var eu: Unit = unit_arrs[enemy_id][i]
 		var is_visible: bool = false
 		
 		
@@ -96,30 +99,27 @@ func get_enemy_unit_visibility(player_id: int) -> Array[Array]:
 				visible_units.append(eu)
 				is_visible = true
 				unit_last_seen_by[pu] = eu
-				units_seen_this_frame[player_id].append(pu)
-				units_seen_this_frame[(player_id+1)%2].append(eu)
+				units_seen_this_frame[player_id][pu] = true
+				units_seen_this_frame[(player_id+1)%2][eu] = true
 				
 				if (visualize_los):
-					DebugDraw3D.draw_line(unit_last_seen_by[eu].LineOfSightTarget.global_position, eu.LineOfSightTarget.global_position, Color(0,255,0), 0.2)
+					DebugDraw3D.draw_line(unit_last_seen_by[eu].LineOfSightTarget.global_position, eu.LineOfSightTarget.global_position, Color(0,255,0, 0.5), 0.2)
 				
-				last_p = unit_arrs[0].size()
+				last_p = unit_arrs[player_id].size()
 				last_e = i+1
 				continue
 			
-		var player_loop_begin_idx: int = last_los_idx_player if first_enemy_iter_this_tick else 0		
+		var player_loop_begin_idx: int = last_los_idx_player[player_id] if first_enemy_iter_this_tick else 0		
 		first_enemy_iter_this_tick = false
 		
-		for j in range(last_los_idx_player, unit_arrs[0].size()):
+		for j in range(last_los_idx_player[player_id], unit_arrs[player_id].size()):
 			
-			if (i==0 && j==0):
-				# Reset frame visibility cache
-				update_closest_visible_enemy_dict(player_id)
-				units_seen_this_frame = [[], []]
+
 				
 
 			
 			last_p = j
-			var pu: Unit = unit_arrs[0][j]
+			var pu: Unit = unit_arrs[player_id][j]
 			
 			if (raycasts_this_tick >= max_raycasts_per_tick):
 				break_enemy = true
@@ -141,16 +141,22 @@ func get_enemy_unit_visibility(player_id: int) -> Array[Array]:
 		if (!is_visible):
 			hidden_units.append(eu)
 	
-	last_los_idx_player = last_p
-	last_los_idx_enemy = last_e
+	last_los_idx_player[player_id] = last_p
+	last_los_idx_enemy[player_id] = last_e
 	
 	# If the player unit index we checked is the last in the array, wrap around to the beginning
-	if (last_los_idx_player >= unit_arrs[0].size()-1):
-		last_los_idx_player = 0
+	if (last_los_idx_player[player_id] >= unit_arrs[player_id].size()-1):
+		last_los_idx_player[player_id] = 0
 		
 		#If the above is true, AND we're at the final enemy unit, wrap it to the beginning as well
-		if (last_los_idx_enemy >= unit_arrs[1].size()-1):
-			last_los_idx_enemy = 0
+		if (last_los_idx_enemy[player_id] >= unit_arrs[enemy_id].size()-1):
+			last_los_idx_enemy[player_id] = 0
+			
+			# update closest enemy dict
+			# Reset frame visibility cache
+			update_closest_visible_enemy_dict(player_id)
+			units_seen_this_frame[0].clear()
+			units_seen_this_frame[1].clear()
 	
 	
 	
@@ -162,8 +168,8 @@ func update_closest_visible_enemy_dict(player_id: int) -> void:
 	
 	unit_closest_visible_enemy.clear()
 	
-	var seen_eus: Array = units_seen_this_frame[(player_id+1)%2].duplicate()
-	var seen_pus: Array = units_seen_this_frame[(player_id)].duplicate()
+	var seen_eus: Array = units_seen_this_frame[(player_id+1)%2].keys().duplicate()
+	var seen_pus: Array = units_seen_this_frame[(player_id)].keys().duplicate()
 	
 	seen_pus.sort_custom(func(a: Unit, b: Unit)->bool: return a.global_position.distance_to(Vector3(0,0,0)) < b.global_position.distance_to(Vector3(0,0,0)))
 	
@@ -174,6 +180,7 @@ func update_closest_visible_enemy_dict(player_id: int) -> void:
 			if (check_los(pu, eu)):
 				unit_last_seen_by[eu] = pu
 				unit_closest_visible_enemy[pu] = eu
+				DebugDraw3D.draw_line(unit_last_seen_by[eu].LineOfSightTarget.global_position, eu.LineOfSightTarget.global_position, Color(0,0,255), 0.2)
 				break
 				
 				
