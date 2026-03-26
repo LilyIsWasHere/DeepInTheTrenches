45d541e542cf1f4716163fa1a0f603e523427bf7
Nick Ralph <nick@theralphs.ca>
2026-03-25 22:35:35 -0400

    Dynamically updating inventory

--- Nick/item_ui.gd
+++ Nick/item_ui.gd
@@ -2,3 +2,5 @@ extends PanelContainer
 
 @onready var itemTexture : TextureRect = $MarginContainer/VBoxContainer/ItemTexture
 @onready var itemLabel : Label = $MarginContainer/VBoxContainer/ItemLabel
+var itemName : String
+var itemAmount : int
