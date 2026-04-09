7e1b034a7b99e278a45aaf72e3e6a881e55f6256
SanPug1 <125669770+SanPug1@users.noreply.github.com>
2026-04-08 21:00:24 -0400

    magazines now stored on player instead of weapon

--- scripts/weapon_scripts/weapon.gd
+++ scripts/weapon_scripts/weapon.gd
@@ -10,6 +10,9 @@ class_name Weapon
 
 # loading the bullet scene
 @export var bullet : PackedScene;
+
+# ammo/mag stuff
+var inventory : Node = null
 const magazineItem : InventoryItem = preload("res://Inventory/InventoryItems/magazine_item.tres")
 const ammoItem : InventoryItem = preload("res://Inventory/InventoryItems/ammo_item.tres")
 
@@ -25,6 +28,10 @@ var enabled : bool = true
 func _ready() -> void:
 	$ReloadTime.wait_time = reload_delay
 	$CooldownTime.wait_time = fire_delay
+	
+	# get parent inventory
+	inventory = get_parent().get_node("Inventory")
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,15 +40,10 @@ func _process(_delta: float) -> void:
 
 # Reloads the magazine using ammo from storage
 func reload() -> void:
-	# Get the amount that needs to be refilled using inventory's max quantity and current quantity
-	var refill_amt : int = $Magazine.get_max_item_quantity(magazineItem) - $Magazine.get_item_quantity(magazineItem)
-	
-	# Get the missing ammo from the storage, this will return 0 if it has enough to fill the mag or return the amount it is missing
-	var missing_ammo : int = $Magazine.remove_items(ammoItem, refill_amt)
-	
-	if missing_ammo < refill_amt:
-		# Add the refill amount - the missing ammo from storage to the magazine (avoids refilling with ammo that isn't there)
-		$Magazine.add_items(magazineItem, refill_amt - missing_ammo)
+	if inventory.get_item_quantity(magazineItem) > 0:
+		# Remove one magazine from unit inventory and load it into the weapon's ammo
+		inventory.remove_items(magazineItem, 1)
+		$Magazine.add_items(ammoItem, $Magazine.get_max_item_quantity(ammoItem))
 		# Start reload timer to avoid shooting while reloading
 		reloading = true
 		$ReloadTime.start()
@@ -53,13 +55,12 @@ func shoot(target_pos : Vector3) -> void:
 	
 	if !reloading and !inWeaponCooldown:
 		# checks that the magazine has enough ammo loaded
-		if $Magazine.get_item_quantity(magazineItem) >= ammo_per_shot:
+		if can_shoot():
 			# check for missing bullets, shouldn't really be necessary but I'm leaving it in for now, incase we only want to check that there is one bullet on the line above
-			var missing_shots : int = $Magazine.remove_items(magazineItem, ammo_per_shot)
+			var missing_shots : int = $Magazine.remove_items(ammoItem, ammo_per_shot)
 			
 			# instantiate a bullet for every shot, will need to setup some kind of spray pattern
 			for i in range(ammo_per_shot - missing_shots):
-				
 				var direction: Vector3 = (target_pos - global_position).normalized()
 				var rand_direction: Vector3 = get_random_gaussian_direction(direction, deg_to_rad(inaccuracy))
 				fire_bullet.rpc(rand_direction)
@@ -92,19 +93,13 @@ func get_random_gaussian_direction(dir: Vector3, sigma: float) -> Vector3:
 	var rand_perp: Vector3 = perpendicular.rotated(newDir, rand_angle)
 	
 	return newDir.rotated(rand_perp, rand_gaussian)
-	
-
-# for loading resources into the weapon inventory, will load ammo into the reserves
-func deposit_ammo(amount : int) -> int:
-	var refill_max : int = $Magazine.get_max_item_quantity(ammoItem) - $Magazine.get_item_quantity(ammoItem)
-	return $Magazine.add_items(ammoItem, amount - refill_max) # return the ammo that doesn't fit in the inventory
 
 
 func _on_reload_time_timeout() -> void:
 	reloading = false
 	
 func can_shoot() -> bool:
-	if $Magazine.get_item_quantity(magazineItem) > 0 or $Magazine.get_item_quantity(ammoItem) > 0:
+	if $Magazine.get_item_quantity(ammoItem) > 0:
 		return true
 	else:
 		return false 
